20665f5e838128c2a26c23361494413c0d975b5c
Your Name <albafermin025@gmail.com>
2024-10-09 13:37:50 -0700

    done with activity 2

---
+++ HW2/Activity 2/java/script.js
@@ -0,0 +1,22 @@
+var isOver21 = true
+var hasAnID = true
+
+if (isOver21 == true && hasAnID == true)
+{
+    console.log("The Patron is allowed in.")
+}
+else{
+    console.log("The Patron is NOT allowed in.")
+}
+
+var GPA = 3.1
+var numberOfUnits = 12
+
+if (GPA >= 3.5 && numberOfUnits >= 12)
+{
+    console.log("You are eligable for the Dean's list.")
+}
+
+else{
+    console.log("You are NOT eligable for the Dean's list.")
+}
