6fa74f1a03d7626827de4e368a29e43b2ece74bf
ferminAJ <albafermin025@gmail.com>
2024-10-17 18:15:36 -0700

    part D done

--- HW3/PartD/java/script.js
+++ HW3/PartD/java/script.js
@@ -1,23 +1,24 @@
-function updateCream(layer, imagePath){
-    const imgElement = document.getElementById(layer);
-    imgElement.src = "assets/" + imagePath;
-    imgElement.style.display = "block";
+function updateCream(layer, imagePath) {
+  const imgElement = document.getElementById(layer);
+  imgElement.src = "assets/" + imagePath;
+  imgElement.style.display = "block";
 }
 
+function hideLayer(layer) {
+  const imgElement = document.getElementById(layer);
+  imgElement.src = "";
+  imgElement.style.display = "none";
+}
 
-
-
-function toggleTopping(toppingID, imagePath){
-    const toppingElement = document.getElementById(toppingID);
-    if (
-        toppingElement.style.display === "none" || toppingElement.style.display === ""
-    )
-    {
-        toppingElement.src = "assets/" + imagePath;
-        toppingElement.style.display = "block";
-    }
-    else
-    {
-        toppingElement.style.display = "none";
-    }
+function toggleTopping(toppingID, imagePath) {
+  const toppingElement = document.getElementById(toppingID);
+  if (
+    toppingElement.style.display === "none" ||
+    toppingElement.style.display === ""
+  ) {
+    toppingElement.src = "assets/" + imagePath;
+    toppingElement.style.display = "block";
+  } else {
+    toppingElement.style.display = "none";
+  }
 }
